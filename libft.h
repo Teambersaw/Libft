d95a9f6abb9db64aaf27bd3e43ca8154629a403e
Rossetti <jules.rossetti@aol.fr>
2022-02-12 15:42:02 +0100

    libft

--- libft.h
+++ libft.h
@@ -6,15 +6,17 @@
 /*   By: teambersaw <teambersaw@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:00:42 by jrossett          #+#    #+#             */
-/*   Updated: 2021/12/03 19:11:24 by teambersaw       ###   ########.fr       */
+/*   Updated: 2022/02/12 15:39:31 by teambersaw       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
 
 typedef struct s_list
 {
@@ -55,6 +57,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char	*dst, const char *src, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin_gnl(char const *s1, char const *s2);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
